0d00b01508f346d690a17c75a6f6e8029d27b018
nave91 <nalekkalapudi@mix.wvu.edu>
2015-06-05 03:27:36 +0000

    removing webmode

---
+++ init.el
@@ -0,0 +1,6 @@
+;;;define current path as load path
+(add-to-list 'load-path "~/.emacs.d/")
+;;;tab as 2 spaces
+(setq-default indent-tabs-mode nil)
+(setq tab-width 2) ; or any other preferred value
+(setq python-indent-offset 2)
